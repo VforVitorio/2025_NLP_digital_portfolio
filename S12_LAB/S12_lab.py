4b0a8a4e3b9a4ca9053111ce7d2df7913e473367
VforVitorio <victorvegasobral@yahoo.com>
2025-04-07 12:47:05 +0200

    added model querying

--- S12_LAB/S12_lab.py
+++ S12_LAB/S12_lab.py
@@ -29,3 +29,46 @@ def get_models():
 
     # We extract and return the model IDs
     return [model[id] for model in data.get("data", [])]
+
+###########################################################################
+
+
+########################### LLM QUERY ##########################
+def query_llm(model, messages, temperature=0.7, max_tokens=2000):
+    """
+    Sends a query to the LLM and gets the response
+
+    Args:
+        model(str): ID of the model to use
+        messages(list): List of message objects in the conversation
+        temperature(float): controls randomnsess in response (0.0-1.0)
+        max_tokens(int): maximum number of tokens in the response
+
+    Returns:
+        dict: JSON response from the LLM and None if error occurs
+    """
+
+    url = f"{BASE_URL}/v1/chat/completions"
+
+    # Prepare payload with all the parameters required
+    payload = {
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+        "max_tokens": max_tokens
+    }
+
+    # Simple error handling in case the model does not process our query correctly
+
+    try:
+        # POST request with our JSON payload
+        response = requests.post(url, json=payload)
+        # Again raise HTTP exception if occurs
+        response.raise_for_status()
+        # Parsing to JSON the response
+        return response.json()
+    except Exception as e:
+        # If request fails we put the error
+        st.error(f"Error querying model: {e}")
+        return None
+#####################################################################
